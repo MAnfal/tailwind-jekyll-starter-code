0e214a1f303dcd72dd0e6705566933ab6e660341
Anfal <muhammadanfal0@gmail.com>
2021-05-15 16:23:21 -0700

    Ignore unwanted stuff

---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+const mix = require("laravel-mix");
+const path = require("path");
+
+mix.setPublicPath("./assets");
+
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src/ts"),
+        },
+    },
+});
+
+if (mix.inProduction()) {
+    mix.disableNotifications();
+}
+
+mix.postCss("src/css/index.css", "assets/css", [require("tailwindcss")]);
+
+mix.ts("src/ts/index.ts", "assets/js");
